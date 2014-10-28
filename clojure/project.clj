979817670da9221ab901dfb22d199483ec7b683d
Oscar Forero <oscar.forero@gatech.edu>
2014-10-28 16:10:09 +0800

    Add support for reading sudoku from a CSV file

--- clojure/project.clj
+++ clojure/project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "MIT License"
             :url "https://raw.githubusercontent.com/oforero/sudoku/master/LICENSE"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+		 [org.clojure/data.csv "0.1.2"]]
   :main ^:skip-aot sudoku.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- clojure/src/sudoku/core.clj
+++ clojure/src/sudoku/core.clj
@@ -1,15 +1,23 @@
 (ns sudoku.core
   (:gen-class)
-  (:use [sudoku.board :only [read-board, show-board-pretty]]
+  (:use [clojure.data.csv :as csv]
+        [clojure.java.io :as io]
+        [sudoku.board :only [read-board, show-board-pretty]]
         [sudoku.types :only [show-value]]
         [sudoku.strategy :only [solve]]))
 
 (defn -main
   "I don't do a whole lot ... yet."
-  [b & args]
-  (println (show-board-pretty (read-board b)))
-  (println (show-board-pretty (solve (read-board b))))
-)
+  ([] (println "Bad parameters, use <-b> <String>"))
+  ([ignore] (println "Bad parameters, use <-b> <String>"))
+  ([f1 in] (cond
+            (= f1 "-b") (do
+                          (println (show-board-pretty (read-board in)))
+                          (println (show-board-pretty (solve (read-board in)))))
+            (= f1 "-csv") (with-open [in-file (io/reader in)]
+                            (let [board (apply str (apply concat (csv/read-csv in-file)))]
+                              (println (show-board-pretty (read-board board)))
+                              (println (show-board-pretty (solve (read-board board)))))))))
 
 (def test-board-1 (read-board "003020600900305001001806400008102900700000008006708200002609500800203009005010300"))
 (def test-board-2 (read-board "200080300060070084030500209000105408000000000402706000301007040720040060004010003"))
